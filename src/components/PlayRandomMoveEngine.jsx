953e390575cb1eb80b134d4e075713388f65ccb0
TadeoCuchman <tadeo.cuchman@gmail.com>
2023-09-26 18:15:54 -0300

    adding engine to code and implementing liches api
    adding engine to code and implementing liches api, lot of code is commented and not used, from here we are seeing what is the best option to use all the weapons that we have to achieve the app

--- src/components/PlayRandomMoveEngine.jsx
+++ src/components/PlayRandomMoveEngine.jsx
@@ -2,16 +2,17 @@
 import { useState, useEffect, useCallback } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
+// import Engine from "./integration/Engine.ts";
 
 export default function PlayRandomMoveEngine({ fen, setFen, setLastMove, setError, validMoves, setValidMoves, setMoveMessage, triggerLineMove, triggerValidationMove }) {
   const [game, setGame] = useState(new Chess());
+  // const engine = useMemo(() => new Engine(), []);
   const [boardOrientation, setBoardOrientation] = useState('white');
   // const [turn, setTurn] = useState('w');
 
   useEffect(() => {
     setFen(game.fen())
   }, [])
-
 
   useEffect(() => {
     if (triggerLineMove != null) {
@@ -25,19 +26,42 @@ export default function PlayRandomMoveEngine({ fen, setFen, setLastMove, setErro
     }
   }, [triggerValidationMove])
 
+  // function findBestMove() {
+  //   engine.evaluatePosition(game.fen(), 10);
+  //   engine.onMessage(({ bestMove }) => {
+  //     if (bestMove) {
+  //       game.move({
+  //         from: bestMove.substring(0, 2),
+  //         to: bestMove.substring(2, 4),
+  //         promotion: bestMove.substring(4, 5),
+  //       });
 
+  //       setChessBoardPosition(game.fen());
+  //     }
+  //   });
+  // }
+ 
   const makeValidationMove = () => {
-    const result = game.move(validMoves);
+    const result = game.move({
+      from: validMoves.substring(0, 2),
+      to: validMoves.substring(2, 4),
+      promotion: validMoves.substring(4, 5),
+    });
     setLastMove({ from: result.from, to: result.to })
-    game.undo(validMoves);
+    game.undo(result);
   }
 
   const makeaButtonMove = (move, nextFen) => {
+    console.log(move, nextFen);
     const newGame = new Chess(nextFen);
     let result = '';
 
     if (move != null) {
-      result = newGame.move(move);
+      result = newGame.move({
+        from: move.substring(0, 2),
+        to: move.substring(2, 4),
+        promotion: move.substring(4, 5),
+      });
       if (result != null) {
         setLastMove({ from: result.from, to: result.to });
       }
@@ -45,6 +69,7 @@ export default function PlayRandomMoveEngine({ fen, setFen, setLastMove, setErro
 
     setFen(newGame.fen())
     setGame(newGame);
+    console.log('>>>>>>>>>>>', newGame.fen())
 
     if (result == null) {
       setError('Impossible move')
@@ -72,7 +97,7 @@ export default function PlayRandomMoveEngine({ fen, setFen, setLastMove, setErro
       };
 
 
-      if (validMoves != null && result != null && ((result?.to == validMoves?.to && result?.from == validMoves?.from) || (result?.to == validMoves) || (result?.san == validMoves))) {
+      if (validMoves != null && result != null && ((result?.to == validMoves?.to && result?.from == validMoves?.from) || (result?.to == validMoves) || (result?.san == validMoves) || (result?.from == validMoves.substring(0,2) && result?.to == validMoves.substring(2,4)) )) {
         setMoveMessage('OK')
         setTimeout(() => {
           setValidMoves(null)
@@ -101,13 +126,13 @@ export default function PlayRandomMoveEngine({ fen, setFen, setLastMove, setErro
 
 
   function onDrop(sourceSquare, targetSquare) {
-    const move = makeAMove({
-      from: sourceSquare,
-      to: targetSquare,
-      promotion: "q",// always promote to a queen for example simplicity
-    });
+  const move = makeAMove({
+  from: sourceSquare,
+  to: targetSquare,
+  promotion: "q",// always promote to a queen for example simplicity
+  });
 
-    return true;
+  return true;
   }
 
   return <Chessboard position={fen} boardOrientation={boardOrientation} customDropSquareStyle={{ backgroundColor: '#FFFFCC' }} onPieceDrop={onDrop} />;
