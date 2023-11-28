a3fb98e88f7f7f94bf9dd92a8216c70ad566cd2f
TadeoCuchman <tadeo.cuchman@gmail.com>
2023-11-28 10:29:47 -0300

    scroll to bottom

--- src/components/MovesBorad.jsx
+++ src/components/MovesBorad.jsx
@@ -1,9 +1,17 @@
 
+import { useRef, useEffect } from "react";
+
 const MovesBoard = ({movesArray}) => {
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+  }, [movesArray]);
 
   return (
     <div style={{ width:'100px', padding:'10px', height: '200px', backgroundColor:'grey', overflow:'auto'}}>
        {movesArray.map((move, index) =>  (<div key={index} style={move.turn == 'w' ? {color:'white'} : {color:'black'}}>{move.move}</div>))}
+       <div ref={bottomRef} />
     </div>
   )
 };
