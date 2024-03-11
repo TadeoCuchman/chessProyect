6f1883589c51829fa021d13e0d36a7baec242dd1
TadeoCuchman <tadeo.cuchman@gmail.com>
2024-03-11 18:55:55 -0300

    fix align

--- src/components/MovesBorad.jsx
+++ src/components/MovesBorad.jsx
@@ -4,14 +4,14 @@ import { useRef, useEffect } from "react";
 const MovesBoard = ({ movesArray }) => {
   const bottomRef = useRef(null);
 
-  useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [movesArray]);
+  // useEffect(() => {
+  //   bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  // }, [movesArray]);
 
   return (
     <div style={{ width: '100px', padding: '10px', height: '200px', backgroundColor: 'grey', overflow: 'auto' }}>
       {movesArray.map((move, index) => (
-        <div key={index}>
+        <div key={index} style={{ textAlign: 'start' }}>
           {index} .
           <span style={move.botMove.turn == 'w' ? { color: 'white' } : { color: 'black' }} className="moveDisplayed">
             {move.botMove.move}
