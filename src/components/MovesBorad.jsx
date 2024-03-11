11b967469452655a1246d19252cd0313c9c460e7
TadeoCuchman <tadeo.cuchman@gmail.com>
2024-03-11 19:17:11 -0300

    reverse moves column

--- src/components/MovesBorad.jsx
+++ src/components/MovesBorad.jsx
@@ -2,27 +2,31 @@
 import { useRef, useEffect } from "react";
 
 const MovesBoard = ({ movesArray }) => {
-  const bottomRef = useRef(null);
+  // const bottomRef = useRef(null);
 
   // useEffect(() => {
   //   bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
   // }, [movesArray]);
 
   return (
-    <div style={{ width: '100px', padding: '10px', height: '200px', backgroundColor: 'grey', overflow: 'auto' }}>
-      {movesArray.map((move, index) => (
-        <div key={index} style={{ textAlign: 'start' }}>
-          {index} .
-          <span style={move.botMove.turn == 'w' ? { color: 'white' } : { color: 'black' }} className="moveDisplayed">
-            {move.botMove.move}
-          </span>
-          -
-          <span style={move.userMove.turn == 'w' ? { color: 'white' } : { color: 'black' }} className="moveDisplayed">
-            {move.userMove.move}
-          </span>
-        </div>))}
-      <div ref={bottomRef} />
-    </div>
+    <div style={{
+      display: 'flex', width: '100px', padding: '10px', height: '200px', backgroundColor: 'grey', overflow: 'auto', flexDirection: 'column-reverse'
+    }}>
+      {
+        movesArray.map((move, index) => (
+          <div key={index} style={{ textAlign: 'start' }}>
+            {index} .
+            <span style={move.botMove.turn == 'w' ? { color: 'white' } : { color: 'black' }} className="moveDisplayed">
+              {move.botMove.move}
+            </span>
+            -
+            <span style={move.userMove.turn == 'w' ? { color: 'white' } : { color: 'black' }} className="moveDisplayed">
+              {move.userMove.move}
+            </span>
+          </div>))
+      }
+      < div />
+    </div >
   )
 };
 
